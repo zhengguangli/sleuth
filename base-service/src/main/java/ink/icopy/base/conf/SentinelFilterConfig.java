e98b3b78231bdceecec4a4ea90f6e5e86babc24d
lizhengguang <zzlxxi@163.com>
2020-04-09 15:59:22 +0800

    增加Sentinel服务限流

---
+++ base-service/src/main/java/ink/icopy/base/conf/SentinelFilterConfig.java
@@ -0,0 +1,25 @@
+package ink.icopy.base.conf;
+
+import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+/**
+ * @author lizhengguang
+ */
+@Configuration
+public class SentinelFilterConfig {
+    @Bean
+    public FilterRegistrationBean sentinelFilterRegistration() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CommonFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("sentinelFilter");
+        registration.setOrder(1);
+
+        return registration;
+    }
+}
